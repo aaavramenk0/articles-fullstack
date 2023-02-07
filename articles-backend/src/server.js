ab995e8225390cadcf859174ff24af92be8fb0fb
Aleksandr <alex.avramenko2004@gmail.com>
2023-02-06 19:45:18 -0500

    Comments in the articles

--- articles-backend/src/server.js
+++ articles-backend/src/server.js
@@ -3,12 +3,15 @@ import express, { request } from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -24,6 +27,19 @@ app.put('/api/articles/:name/upvote', (req, res) => {
         res.send(`The ${name} article doesn\'t exist`);
     }
 });
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    
+    const article = articlesInfo.find(a => a.name === name);
+    if(article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send(`The article doesn\'t exist`)
+    }
+})
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
